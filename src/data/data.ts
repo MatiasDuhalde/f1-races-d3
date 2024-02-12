154e12da1996e562b02597ac2e06b7937c96eaa1
Matías Duhalde <contact@duhal.de>
2024-02-12 09:16:56 +0100

    feat: add local cache to data service

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,13 +3,21 @@ import type { Circuit, Driver, Race, Season } from './types';
 import { dateOrUndefined, numberOrUndefined, stringOrUndefined } from './utils';
 
 export class DataService {
-  private static readonly BASE_PATH = '/data/f1/';
+  private static readonly BASE_PATH =
+    'https://raw.githubusercontent.com/MatiasDuhalde/f1-races-d3/master/data/';
+  private static readonly F1_PREFIX = 'f1/';
+  private static readonly GEO_PREFIX = 'geo/';
   private static readonly CSV_EXTENSION = '.csv';
+  private static readonly JSON_EXTENSION = '.json';
 
-  private static readonly CIRCUITS_PATH = this.BASE_PATH + 'circuits' + this.CSV_EXTENSION;
-  private static readonly DRIVERS_PATH = this.BASE_PATH + 'drivers' + this.CSV_EXTENSION;
-  private static readonly RACES_PATH = this.BASE_PATH + 'races' + this.CSV_EXTENSION;
-  private static readonly SEASONS_PATH = this.BASE_PATH + 'seasons' + this.CSV_EXTENSION;
+  private static readonly CIRCUITS_PATH =
+    this.BASE_PATH + this.F1_PREFIX + 'circuits' + this.CSV_EXTENSION;
+  private static readonly DRIVERS_PATH =
+    this.BASE_PATH + this.F1_PREFIX + 'drivers' + this.CSV_EXTENSION;
+  private static readonly RACES_PATH =
+    this.BASE_PATH + this.F1_PREFIX + 'races' + this.CSV_EXTENSION;
+  private static readonly SEASONS_PATH =
+    this.BASE_PATH + this.F1_PREFIX + 'seasons' + this.CSV_EXTENSION;
 
   // private static readonly CONSTRUCTOR_RESULTS_PATH =
   //   this.BASE_PATH + 'constructor_results' + this.CSV_EXTENSION;
@@ -26,7 +34,17 @@ export class DataService {
   //   this.BASE_PATH + 'sprint_results' + this.CSV_EXTENSION;
   // private static readonly STATUS_PATH = this.BASE_PATH + 'status' + this.CSV_EXTENSION;
 
+  private static readonly WORLD_MAP_PATH =
+    this.BASE_PATH + this.GEO_PREFIX + 'world_map' + this.JSON_EXTENSION;
+
   private static instance: DataService;
+
+  // cache
+  private circuits: Circuit[] | undefined = undefined;
+  private drivers: Driver[] | undefined = undefined;
+  private seasons: Season[] | undefined = undefined;
+  private races: Race[] | undefined = undefined;
+  private worldMap: d3.ExtendedFeatureCollection | undefined = undefined;
 
   private constructor() {}
 
@@ -38,9 +56,9 @@ export class DataService {
   }
 
   public async getCircuits(): Promise<Circuit[]> {
-    const result = await d3.csv(DataService.CIRCUITS_PATH);
-    return result.map((d) => {
-      return {
+    if (this.circuits === undefined) {
+      const result = await d3.csv(DataService.CIRCUITS_PATH);
+      this.circuits = result.map((d) => ({
         circuitId: +d.circuitId,
         circuitRef: d.circuitRef,
         name: d.name,
@@ -50,14 +68,15 @@ export class DataService {
         lng: +d.lng,
         alt: numberOrUndefined(d.alt),
         url: d.url,
-      };
-    });
+      }));
+    }
+    return this.circuits;
   }
 
   public async getDrivers(): Promise<Driver[]> {
-    const result = await d3.csv(DataService.DRIVERS_PATH);
-    return result.map((d) => {
-      return {
+    if (this.drivers === undefined) {
+      const result = await d3.csv(DataService.DRIVERS_PATH);
+      this.drivers = result.map((d) => ({
         driverId: +d.driverId,
         driverRef: d.driverRef,
         number: numberOrUndefined(d.number),
@@ -67,24 +86,26 @@ export class DataService {
         dob: new Date(d.dob),
         nationality: d.nationality,
         url: d.url,
-      };
-    });
+      }));
+    }
+    return this.drivers;
   }
 
   public async getSeasons(): Promise<Season[]> {
-    const result = await d3.csv(DataService.SEASONS_PATH);
-    return result.map((d) => {
-      return {
+    if (this.seasons === undefined) {
+      const result = await d3.csv(DataService.SEASONS_PATH);
+      this.seasons = result.map((d) => ({
         year: +d.year,
         url: d.url,
-      };
-    });
+      }));
+    }
+    return this.seasons;
   }
 
   public async getRaces(): Promise<Race[]> {
-    const result = await d3.csv(DataService.RACES_PATH);
-    return result.map((d) => {
-      return {
+    if (this.races === undefined) {
+      const result = await d3.csv(DataService.RACES_PATH);
+      this.races = result.map((d) => ({
         raceId: +d.raceId,
         year: +d.year,
         round: +d.round,
@@ -103,11 +124,15 @@ export class DataService {
         qualiTime: stringOrUndefined(d.qualiTime),
         sprintDate: dateOrUndefined(d.sprintDate),
         sprintTime: stringOrUndefined(d.sprintTime),
-      };
-    });
+      }));
+    }
+    return this.races;
   }
 
   public async getWorldMapGeoJson(): Promise<d3.ExtendedFeatureCollection> {
-    return d3.json('/data/geo/world_map.json') as Promise<d3.ExtendedFeatureCollection>;
+    if (this.worldMap === undefined) {
+      this.worldMap = (await d3.json(DataService.WORLD_MAP_PATH)) as d3.ExtendedFeatureCollection;
+    }
+    return this.worldMap;
   }
 }
